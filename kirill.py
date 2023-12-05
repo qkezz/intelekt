15089d72284aefd74bf3286ac07baf2fd277d53c
Kirill K <ihateqkezz@gmail.com>
2023-12-05 20:19:02 +0300

    Update kirill.py

--- kirill.py
+++ kirill.py
@@ -1,1 +1,8 @@
 import telebot
+
+token = "6813041632:AAFDdSD7f5-Rv_vjFrHFPHsxJKcgqYge0Lk"
+bot=telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет ✌️ ")
+bot.infinity_poling()
